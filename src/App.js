50d7addc64a9ae977f7e79e0730e592437c3036c
Zoha Ilyas <zoha.ilyas4@gmail.com>
2020-06-07 01:48:09 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br/> Update by Zoha Ilyas
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App({name, age}) {
+   
+
+  return <div>
+            Hello from <strong>{name}</strong>, This is my age Age = {age - 5} <br/>
+            <Hello firstName={name}> </Hello>
+            <div>
+              <h1>Added headings</h1>
+              <hr/>           
+              <h3>Ordered List</h3>
+              <li>NO 1</li>
+              <li>NO 2</li>
+              <li>NO 3</li>
+
+              <br/>
+              <h3>Unordered List</h3>
+      
+              <li>NO 1</li>
+              <li>NO 2</li> 
+              <li>NO 3</li>
+              
+              <br/>
+              Adding 2 and 5 and 7 = {2+5+7}
+              <br/>
+              Multiply 2 and 6 = {2*6}
+
+            </div>
+            
+         </div>
+   
+  }
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import './Hello.css';
+
+function Hello({firstName}) {
+  return <p className="myname">PIAIC STUDENT : {firstName}</p>
+ 
+  }
+
+export default Hello;
